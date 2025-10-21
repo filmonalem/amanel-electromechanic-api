c387b9b3ca8fffcb52dc43ec03666e3ced5ad185
filmon <filmonalem321@gmail.com>
2025-10-21 11:32:55 +0300

    up

--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -10,12 +10,11 @@ import { TypeOrmModule } from '@nestjs/typeorm';
       useFactory: async (configService: ConfigService) => {
         const databaseUrl = configService.get('DATABASE_URL');
         if (databaseUrl) {
-          // Parse the DATABASE_URL
           const parsedUrl = new URL(databaseUrl);
           return {
             type: 'postgres',
             host: parsedUrl.hostname,
-            port: parseInt(parsedUrl.port),
+            port: parsedUrl.port ? parseInt(parsedUrl.port) : 5432,
             username: parsedUrl.username,
             password: parsedUrl.password,
             database: parsedUrl.pathname.split('/')[1],
@@ -27,6 +26,8 @@ import { TypeOrmModule } from '@nestjs/typeorm';
               idleTimeoutMillis: 30000,
               connectionTimeoutMillis: 2000,
             },
+            retryAttempts: 5,
+            retryDelay: 3000,
           };
         } else {
           return {
@@ -34,7 +35,7 @@ import { TypeOrmModule } from '@nestjs/typeorm';
             host:
               configService.get('DB_HOST') ||
               'dpg-cqj8l8unttvs73bkjvv0-a.oregon-postgres.render.com',
-            port: 5432,
+            port: configService.get<number>('DB_PORT') || 5432,
             username: configService.get('DB_USERNAME') || 'Philmon',
             password: configService.get('DB_PASSWORD') || 'f3451146',
             database: configService.get('DB_NAME') || 'loop',
@@ -46,6 +47,8 @@ import { TypeOrmModule } from '@nestjs/typeorm';
               idleTimeoutMillis: 30000,
               connectionTimeoutMillis: 2000,
             },
+            retryAttempts: 5,
+            retryDelay: 3000,
           };
         }
       },
