8cdbf23a709a4d26c8344fd6423527d04185d21e
filmon <filmonalem321@gmail.com>
2025-10-21 11:24:46 +0300

    db connection

--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -1,27 +1,54 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
+
 @Module({
   imports: [
     TypeOrmModule.forRootAsync({
       imports: [ConfigModule],
       inject: [ConfigService],
-      useFactory: async (configService: ConfigService) => ({
-        type: 'postgres',
-        host: 'dpg-cqj8l8unttvs73bkjvv0-a.oregon-postgres.render.com',
-        port: 5432,
-        username: configService.get('DB_USERNAME') || 'Philmon',
-        password: configService.get('DB_PASSWORD') || 'f3451146',
-        database: configService.get('DB_NAME') || 'loop',
-        entities: [__dirname + '/../**/*.entity{.ts,.js}'],
-        synchronize: configService.get('NODE_ENV') !== 'production',
-        ssl: { rejectUnauthorized: false }, // Required for Render
-        extra: {
-          max: 20, // pool size
-          idleTimeoutMillis: 30000,
-          connectionTimeoutMillis: 2000,
-        },
-      }),
+      useFactory: async (configService: ConfigService) => {
+        const databaseUrl = configService.get('DATABASE_URL');
+        if (databaseUrl) {
+          // Parse the DATABASE_URL
+          const parsedUrl = new URL(databaseUrl);
+          return {
+            type: 'postgres',
+            host: parsedUrl.hostname,
+            port: parseInt(parsedUrl.port),
+            username: parsedUrl.username,
+            password: parsedUrl.password,
+            database: parsedUrl.pathname.split('/')[1],
+            entities: [__dirname + '/../**/*.entity{.ts,.js}'],
+            synchronize: configService.get('NODE_ENV') !== 'production',
+            ssl: { rejectUnauthorized: false },
+            extra: {
+              max: 20,
+              idleTimeoutMillis: 30000,
+              connectionTimeoutMillis: 2000,
+            },
+          };
+        } else {
+          return {
+            type: 'postgres',
+            host:
+              configService.get('DB_HOST') ||
+              'dpg-cqj8l8unttvs73bkjvv0-a.oregon-postgres.render.com',
+            port: 5432,
+            username: configService.get('DB_USERNAME') || 'Philmon',
+            password: configService.get('DB_PASSWORD') || 'f3451146',
+            database: configService.get('DB_NAME') || 'loop',
+            entities: [__dirname + '/../**/*.entity{.ts,.js}'],
+            synchronize: configService.get('NODE_ENV') !== 'production',
+            ssl: { rejectUnauthorized: false },
+            extra: {
+              max: 20,
+              idleTimeoutMillis: 30000,
+              connectionTimeoutMillis: 2000,
+            },
+          };
+        }
+      },
     }),
   ],
 })
