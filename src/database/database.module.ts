e7a069f8b10d18764a846c1a11fad4bc03c0cba0
filmon <filmonalem321@gmail.com>
2025-10-21 10:51:40 +0300

    db connection

--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -9,14 +9,27 @@ import { TypeOrmModule } from '@nestjs/typeorm';
       inject: [ConfigService],
       useFactory: async (configService: ConfigService) => ({
         type: 'postgres',
-        host: configService.get('DB_HOST'),
-        port: configService.get('DB_PORT'),
-        username: configService.get('DB_USERNAME'),
-        password: configService.get('DB_PASSWORD'),
-        database: configService.get('DB_NAME'),
+        host:
+          configService.get('DB_HOST') ||
+          'dpg-cqj8l8unttvs73bkjvv0-a.oregon-postgres.render.com',
+        port: configService.get<number>('DB_PORT') || 5432,
+        username: configService.get('DB_USERNAME') || 'loop_db_user',
+        password: configService.get('DB_PASSWORD') || 'f3451146',
+        database: configService.get('DB_NAME') || 'loop',
         entities: [__dirname + '/../**/*.entity{.ts,.js}'],
+        synchronize: configService.get('NODE_ENV') !== 'production',
 
-        synchronize: true,
+        ssl: true,
+        extra: {
+          ssl: {
+            rejectUnauthorized: false,
+            max: 20,
+            idleTimeoutMillis: 30000,
+            connectionTimeoutMillis: 2000,
+          },
+        },
+
+        poolSize: 10,
       }),
     }),
   ],
