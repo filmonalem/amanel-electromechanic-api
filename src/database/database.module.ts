b25f15c2b73ebff3d7d53435f6e9b6905aa4e742
filmon <filmonalem321@gmail.com>
2025-10-21 11:14:12 +0300

    database update

--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -1,7 +1,6 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
-
 @Module({
   imports: [
     TypeOrmModule.forRootAsync({
@@ -9,27 +8,19 @@ import { TypeOrmModule } from '@nestjs/typeorm';
       inject: [ConfigService],
       useFactory: async (configService: ConfigService) => ({
         type: 'postgres',
-        host:
-          configService.get('DB_HOST') ||
-          'dpg-cqj8l8unttvs73bkjvv0-a.oregon-postgres.render.com',
-        port: configService.get<number>('DB_PORT') || 5432,
-        username: configService.get('DB_USERNAME') || 'loop_db_user',
+        host: 'dpg-cqj8l8unttvs73bkjvv0-a.oregon-postgres.render.com',
+        port: 5432,
+        username: configService.get('DB_USERNAME') || 'Philmon',
         password: configService.get('DB_PASSWORD') || 'f3451146',
         database: configService.get('DB_NAME') || 'loop',
         entities: [__dirname + '/../**/*.entity{.ts,.js}'],
         synchronize: configService.get('NODE_ENV') !== 'production',
-
-        ssl: true,
+        ssl: { rejectUnauthorized: false }, // Required for Render
         extra: {
-          ssl: {
-            rejectUnauthorized: false,
-            max: 20,
-            idleTimeoutMillis: 30000,
-            connectionTimeoutMillis: 2000,
-          },
+          max: 20, // pool size
+          idleTimeoutMillis: 30000,
+          connectionTimeoutMillis: 2000,
         },
-
-        poolSize: 10,
       }),
     }),
   ],
