fa15895a152adeb7312f4fb4f3dbd798d5695c16
filmon <filmonalem321@gmail.com>
2025-10-23 22:17:50 +0300

    add

--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -11,7 +11,7 @@ import { ConfigModule, ConfigService } from '@nestjs/config';
         type: 'postgres',
         url: configService.get<string>('DATABASE_URL'),
         autoLoadEntities: true,
-        synchronize: true,
+        synchronize: false,
         ssl: {
           rejectUnauthorized: false,
         },
