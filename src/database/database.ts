21f34304a1e5c0f6f4b31c929a8dd0ead70b371f
filmon <filmonalem321@gmail.com>
2026-03-28 14:55:23 +0300

    database

--- src/database/database.ts
+++ src/database/database.ts
@@ -1,21 +1,3 @@
-import { Module } from '@nestjs/common';
-import { TypeOrmModule } from '@nestjs/typeorm';
-
-@Module({
-  imports: [
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: 'localhost',
-      port: 5432,
-      username: 'philmon',
-      password: 'f3451146',
-      database: 'website',
-      autoLoadEntities: true, // ✅ safest
-      synchronize: true,
-    })
-  ],
-})
-export class DatabaseModule { }
 // import { Module } from '@nestjs/common';
 // import { TypeOrmModule } from '@nestjs/typeorm';
 
@@ -23,13 +5,31 @@ export class DatabaseModule { }
 //   imports: [
 //     TypeOrmModule.forRoot({
 //       type: 'postgres',
-//       url: process.env.DATABASE_URL, // ✅ important
-//       autoLoadEntities: true,
-//       synchronize: true, 
-//       ssl: {
-//         rejectUnauthorized: false, // ✅ required for Render
-//       },
-//     }),
+//       host: 'localhost',
+//       port: 5432,
+//       username: 'philmon',
+//       password: 'f3451146',
+//       database: 'website',
+//       autoLoadEntities: true, // ✅ safest
+//       synchronize: true,
+//     })
 //   ],
 // })
-// export class DatabaseModule {}
+// export class DatabaseModule { }
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+@Module({
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      url: process.env.DATABASE_URL, // ✅ important
+      autoLoadEntities: true,
+      synchronize: true, 
+      ssl: {
+        rejectUnauthorized: false, // ✅ required for Render
+      },
+    }),
+  ],
+})
+export class DatabaseModule {}
